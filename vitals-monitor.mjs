4ac6f11f4c00cad0d33163b7b4487332c078f185
Sushant <sushantpardhi186@gmail.com>
2025-08-20 16:03:08 +0530

    Update vitals-monitor.mjs

--- vitals-monitor.mjs
+++ vitals-monitor.mjs
@@ -35,7 +35,7 @@ function vitalStatusChecker(temperature, pulseRate, spo2){
   }
 }
 
-async function vitals_ok(temperature,pulseRate,spo2) {
+async function vitalsOk(temperature,pulseRate,spo2) {
     const [status, message] = vitalStatusChecker(temperature,pulseRate,spo2);
 
     if(!status){
@@ -46,4 +46,5 @@ async function vitals_ok(temperature,pulseRate,spo2) {
     return status;
     
 }
+
 
